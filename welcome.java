c562535015976da9c5447b0acf57fac161f7e822
Moby Okitikpi <mogbitseo@yahoo.com>
2019-09-04 16:39:02 +0100

    changed the welcome file

--- welcome.java
+++ welcome.java
@@ -1,7 +1,7 @@
 class Welcome{
    	void message(){
 		//System.out.print("Hello");
-		//System.out.print("Boy");
+		System.out.print("Boy");
 		//System.out.print("Hello dear tell me how its going to be \rI day ahead of");
 		System.out.println("Hello dude as e dey go \\ we go see for that side");
 		System.out.print (" \"So it is what it is\" ");
